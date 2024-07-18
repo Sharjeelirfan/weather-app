3433ba130a9e7381173c80e1e25a9be08c5d65db
sharjeel <sharjeelkhatri303@gmail.com>
2024-07-18 12:34:09 +0500

    complete

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const input = document.getElementById("input");
 const button = document.getElementById("btn");
-const form = document.getElementById("form");
 
 const celciusTemp = document.getElementById("celcius");
 const cityNameloc = document.getElementById("loc");
@@ -9,40 +8,40 @@ const text = document.getElementById("wethr-text");
 const icon = document.getElementById("wethr-icon");
 const countryName = document.getElementById("temp");
 
+// let cityName = 'karachi'
+
 async function fetchData(cityName) {
   try {
     const promise = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=c2184b199a854aa9bb094105242105&q=${cityName}&aqi=no
-      `
+      `https://api.weatherapi.com/v1/current.json?key=537650cb9d644b15b87145122240203&q=${cityName}`
     );
-    return await promise.json();  
+    return await promise.json();
   } catch (error) {
     console.error(error);
-  } 
+  }
 }
 
-form.addEventListener("submit", async (event) => {
+button.addEventListener("click", async (event) => {
   event.preventDefault();
-  let value = input.value.trim();
+  // console.log(event.target);
+  const value = input.value.trim();
   if (value !== "") {
+    // console.log(fetchData(value));
     const result = await fetchData(value);
     // console.log(result);
-    let celcius = `${result.current.temp_c}°C`;
+    let celcius = `${result.current.temp_c}`;
     let city = `${result.location.name}`;
     let date = `${result.location.localtime}`;
     let condition_text = `${result.current.condition.text}`;
-    // let condition_icon = `${result.current.condition.icon}`
+    let condition_icon = `${result.current.condition.icon}`;
     let country = `${result.location.country}`;
     // console.log(country);
-    
+
     celciusTemp.innerText = `${celcius}`;
     cityNameloc.innerText = `${city}`;
     date_time.innerText = `${date}`;
     text.innerText = `${condition_text}`;
-    // icon.innerText = `${condition_icon}`
+    icon.src = `${condition_icon}`;
     countryName.innerText = `${country}`;
-
-    input.value = ""
-
   }
 });
